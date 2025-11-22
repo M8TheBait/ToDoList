4fa10f66b67540c8c47f5b3c8c7e0e72ae90b85d
Oskar Andersson <oskarandersson0111@gmail.com>
2025-11-22 17:23:31 +0100

    Finished MVP

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,12 @@ public class Main {
 
         Scanner scanner = new Scanner (System.in);
         int choice = 0;
+        String task;
+        int remove = 0;
 
         // initialize list
 
-        ArrayList<String> toDo = new ArrayList<>();
+        ArrayList<String> toDos = new ArrayList<>();
 
         // Present options through a do-while loop
 
@@ -24,10 +26,50 @@ public class Main {
             System.out.println("4. Exit");
             System.out.print("Enter a choice: ");
             choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(choice){
+                case 1 -> {
+                    System.out.println("Write the task you would like to add below: ");
+                    task = scanner.nextLine();
+                    toDos.add(task);
+                    System.out.println("task added");
+                }
+                case 2 -> {
+                    if(toDos.isEmpty()){
+                        System.out.println("The list is empty");
+                    }
+                    else{
+                        for(int i = 0; i < toDos.size(); i++){
+                            System.out.println((i+1) + ". " + toDos.get(i));
+                        }
+                    }
+                }
+                case 3 -> {
+                    if(toDos.isEmpty()){
+                        System.out.println("The list is empty");
+                    }
+                    else {
+                        System.out.println("Enter the task number you want to remove: ");
+                        for(int i = 0; i < toDos.size(); i++){
+                            System.out.println((i+1) + ". " + toDos.get(i));
+                        }
+                        remove = scanner.nextInt();
+                        scanner.nextLine();
+                        if(remove > 0 && remove <= toDos.size()){
+                            toDos.remove(remove - 1);
+                            System.out.println("Task removed.");
+                        }
+                        else{
+                            System.out.println("Invalid Task number");
+                        }
+                    }
+                }
+                case 4 -> System.out.println("Closing program...");
+                default -> System.out.println("Invalid choice! Enter between 1-4");
+            }
         } while(choice != 4);
 
-        // Add components to list using user input
-        // checkbox to list
-        // complete list
+        scanner.close();
     }
 }
